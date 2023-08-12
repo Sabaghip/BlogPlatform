864ae960c7b5cc05e41264a12a11538ca0ef0ab3
sabaghip <sabaghi.p@gmail.com>
2023-08-12 20:57:17 +0330

    feat : added postgres config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    UsersModule,
+  ],
 })
 export class AppModule {}
