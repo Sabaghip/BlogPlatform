ba3b1f0469c119695fc28d3b9956779eee13cf96
sabaghip <sabaghi.p@gmail.com>
2023-08-14 13:57:39 +0330

    feat : added guard to user controller

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,9 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { PostService } from './post.service';
 
 @Controller('post')
+@UseGuards(AuthGuard())
 export class PostController {
     constructor(
-        private postService : PostService
+        private postService : PostService,
     ){}
+
+    @Post("/test")
+    test(@Query() q){
+        console.log(q);
+    }
 }
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from 'src/users/users.module';
 import { PostController } from './post.controller';
 import { PostRepository } from './post.repository';
 import { PostService } from './post.service';
 
 @Module({
-  imports : [TypeOrmModule.forFeature([PostRepository]),],
+  imports : [TypeOrmModule.forFeature([PostRepository]), UsersModule],
   controllers: [PostController],
   providers: [PostService, PostRepository]
 })
